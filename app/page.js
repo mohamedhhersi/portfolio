9c54116de111f7734ea4fdfa4a877b04aef4e4b3
Mohamed <mohamedhersidev@gmail.com>
2025-01-08 03:07:20 -0500

    included new components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import Hero from "../components/Hero";
+import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import Container from "@/components/Container";
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Hero />
-    </>
+      <Skills />
+      <Projects />
+    </Container>
   );
 }
