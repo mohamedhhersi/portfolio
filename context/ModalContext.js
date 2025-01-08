0375a8485e1b831c43a7c440178b206ed5ff78f8
Mohamed <mohamedhersidev@gmail.com>
2025-01-08 03:09:21 -0500

    added modal open close functionality

---
+++ context/ModalContext.js
@@ -0,0 +1,18 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const ModalContext = createContext();
+
+export function ModalProvider({ children }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <ModalContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+export function useModal() {
+  return useContext(ModalContext);
+}
