3bbeb52148d6cffe4aa9ababff63e75966ceac01
Mohamed <mohamedhersidev@gmail.com>
2025-01-08 03:07:08 -0500

    added modal,footer components

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer";
+import { ModalProvider } from "@/context/ModalContext";
+import ScrollToTop from "@/components/ScrollToTop";
 
 export const metadata = {
   title: "Your Portfolio",
@@ -9,9 +12,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className="bg-gray-50">
-        <Navbar />
-        <main>{children}</main>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        />
+      </head>
+      <body>
+        <ModalProvider>
+          <ScrollToTop />
+          <Navbar />
+          {children}
+          <Footer />
+        </ModalProvider>
       </body>
     </html>
   );
